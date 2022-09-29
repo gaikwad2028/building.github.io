34d16fc3d045cf5ce71e4449f535f940d0e2d137
gaikwad2028 <108988732+gaikwad2028@users.noreply.github.com>
2022-09-30 04:45:15 +0530

    first

---
+++ design/algorithm.js
@@ -0,0 +1,527 @@
+
+// edabit 
+// algorithm
+
+// function sumOfdigits(a,b){
+
+//   return(a+b)
+// }
+
+// let a= Number(prompt("enter number?"));
+
+// let b= Number(prompt("enter number?"));
+
+// sumOfdigits(a,b)
+
+//  console.log(sumOfdigits(a,b))
+
+
+
+// function sumofNext(num) {
+
+//     return(num+1)
+    
+// }
+
+// let num=Number(prompt("enter number?"));
+// sumofNext(num)
+
+// console.log(sumofNext(num))
+
+// convert years into days
+
+// function ageTodays(a){
+
+//     return(a*365)
+
+// }
+// let a=Number(prompt("enter number?"));
+// ageTodays(a)
+// console.log(ageTodays(a))
+
+
+
+// square roots
+
+// function squareRoots(a){
+
+//         return(a*a)
+// }
+
+// let a=Number(prompt("enter number?"));
+// squareRoots(a)
+// console.log(squareRoots(a))
+
+// footballpoints
+
+// function pointsTabel(wins,draw,loss){
+
+//     return((wins*3)+(draw*1)+loss*0)
+// }
+// let wins=Number(prompt("enter wins?"));
+// let draw=Number(prompt("enter draws?"));
+// let loss=Number(prompt("enter losses ?"));
+// pointsTabel(wins,draw,loss)
+
+// console.log(pointsTabel(wins,draw,loss))
+
+// Create a function that will handle simple math expressions. The input is an expression in the form of a string.
+
+// function calCofdigits(a,b) {
+//     return()
+// }
+
+// Multiple of 100
+
+// function multiPle(a) {
+
+//     if(a%100==0){
+//         return(true)
+//     }else{
+//         return(false)
+//     }
+    
+// }
+// let a=Number(prompt("enter number?"));
+// multiPle(a)
+// console.log(multiPle(a))
+
+
+// Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+
+
+
+// function areaOfrectangel(a,b) {
+    
+    
+
+//     if(a<=0 || b<=0){
+//         return(-1)
+//     }
+//     else{
+//         return(a*b)
+//     }
+// }
+
+// let a=Number(prompt("enter length?"));
+// let b=Number(prompt("enter breadth?"));
+
+// areaOfrectangel(a,b) 
+
+// console.log(areaOfrectangel(a,b))
+
+
+// Leap Year Function
+
+// function leapYear(year) {
+
+//     if((0 == year % 4) && (0 != year % 100) || (0 == year % 400)){
+//         return(year+  " "+ "true")
+//     }
+//     else{
+//         return(year+  " "+ "true")
+//     }
+    
+// }
+
+// let year=Number(prompt("enter year?"));
+// leapYear(year)
+
+// console.log(leapYear(year))
+
+
+// function imposterFormula(a,b) {
+
+//     return(Math.ceil(a/b*100)+"%")
+// }
+
+// let a =Number(prompt("enter imposter?"));
+// let b=Number(prompt("enter peopel?"));
+
+// imposterFormula(a,b)
+
+// console.log(imposterFormula(a,b))
+
+// function stackBoxes(a) {
+
+//    if (a===1){
+//     return(1)
+//    }
+//    else if(a===0){
+//     return(0)
+//    } else{
+//     for(i=2;i<=a;i++){
+        
+//         return(Math.pow(i,a))
+//     }
+//    }
+
+//    }
+     
+
+
+// let a =Number(prompt("enter stack?"));
+// stackBoxes(a)
+// console.log(stackBoxes(a))
+
+// function sticKers(a) {
+
+//     return (6 * Math.pow(a,2));
+    
+// }
+// let a =Number(prompt("enter stack?"));
+// sticKers(a)
+// console.log(sticKers(a))
+
+
+// function fiftyThirtyTwenty(a) {
+
+   
+//     return(("NEEDS"+":"+a*50/100) +" " +("WANTS"+":"+a*30/100)+" " + ("SAVINGS"+":"+a*20/100)) 
+// }
+// let a =Number(prompt("enter amount"));
+// fiftyThirtyTwenty(a)
+// console.log(fiftyThirtyTwenty(a))
+
+
+// function equalSlices(totalslices, norecipients, sliceseach){
+
+//   if(norecipients*sliceseach<=totalslices){
+//     return(true)
+//   }
+//   else{
+//     return(false)
+//   }
+
+// }
+
+// let totalslices=Number(prompt("enter totalslices"));
+// let norecipients=Number(prompt("enter peopel"));
+// let sliceseach=Number(prompt("enter sliceseach"));
+
+// equalSlices(totalslices, norecipients, sliceseach)
+
+
+// console.log(equalSlices(totalslices, norecipients, sliceseach))
+
+
+// function sumOfcubes(a,b,c) {
+       
+//     return(a**3+b**3+c**3)
+//     }
+    
+
+// let a=Number(prompt("enter totalslices"));
+// let b=Number(prompt("enter peopel"));
+// let c=Number(prompt("enter sliceseach"));
+
+// sumOfcubes(a,b,c)
+
+// console.log(sumOfcubes(a,b,c))
+
+
+// function matchHouses(step) {
+// 	if(step === 0) {
+// 		return 0;
+// 	}else{
+// 		return (step*6)-(step -1);
+// 	}
+// }
+
+// let step= Number(prompt("enter?"))
+
+// matchHouses(step)
+
+// console.log(matchHouses(step))
+
+// function squareAreasDifference(r) {
+// 	return 2 * r ** 2;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// logic
+
+// function Checked(a,b){
+
+//     if(a==1 && b==1){
+//         return(true)
+//     }else if(a!=1 && b==1){
+//         return(false)
+//     }else if(a==1 && b!=1){
+//         return(false)
+//     }else {
+//               return(false)
+//     }
+
+// }
+
+// let a=Number(prompt("enter a?"))
+// let b=Number(prompt("enter b?"))
+// Checked(a,b)
+
+// console.log(Checked(a,b))
+
+
+
+// function Checked(a,b){
+
+//     if(a==b){
+//         return(typeof(a,b))
+//     }
+
+// }
+// let a="1"
+// let b= "akash"
+
+// Checked(a,b)
+
+// console.log(Checked(a,b))
+
+
+
+// function Negative(a){
+//     return(-a)
+// }
+
+// let a=15
+// console.log(Negative(a))
+
+
+
+// function drinks(age,onbreak) {
+//     if(age>=18 && onbreak==true){
+//         return(true)
+//     }else{
+//         return(false)
+//     }
+// }
+
+// let age= Number(prompt("age?"))
+// let onbreak= (prompt("break?"))
+
+// console.log(drinks(age,onbreak))
+
+// function  Swapping(a){
+    
+//     return(a/10>a%10)
+// }
+
+// let a= Number(prompt("age?"))
+// Swapping(a)
+// console.log(Swapping(a))
+
+
+// function Flip(a) {
+    
+
+//     if(a==0){
+//         return(1)
+//     }
+//     else{
+//         return(0)
+//     }
+// }
+
+
+
+// let a=1;
+// console.log(Flip(a) )
+
+
+
+// function perimeter(s,c){
+//     return(("S"+" "+4*s)+" "+("C"+" "+6.28*c))
+   
+// }
+
+// let s= Number(prompt("square?"));
+// let c= Number(prompt("circel?"))
+// perimeter(s,c)
+// console.log(perimeter(s,c))
+
+
+// function generation(a,b) {
+
+//     for(i=0;i<=a;i++){
+//         let value=" "
+        
+//          if((a===0 ) && (b=="f")){
+//             value="me"+b
+//             return(value||i);
+//          }
+        
+//     }
+    
+// }
+
+// let a=3
+// let b="f"
+
+// generation(a,b)
+
+// console.log(generation(a,b))
+// return y=="m"?gen[x][0]:gen[x][1];
+// }
+
+
+// function repeatString(txt, n) {
+
+// if(n>0)
+//     return txt.repeat(n);
+//   else
+//     return "";
+// }
+	
+
+// let txt="akash";
+// let n=3;
+
+// let a=repeatString(txt, n)
+// console.log(repeatString(txt, n))
+
+
+
+// function logicGates(a,b) {
+
+//     if (a==b){
+//         return(a)
+//     }
+//     return(false)
+    
+// }
+// let a=2;
+// let b=2;
+// let ans=logicGates(a,b)
+// console.log(logicGates(a,b))
+
+
+// function whoWins(amount,inventory,price,size) {
+
+//     if(amount/price>inventory/size){
+//         return("Bill")
+//     }
+//     return("will")
+// }
+    
+
+
+// let amount=Number(prompt("enter amount ?"))
+// let inventory=Number(prompt("enter inventory ?"))
+// let price=Number(prompt("enter price ?"))
+// let size=Number(prompt("enter size?"))
+
+// let ans =whoWins(amount,inventory,price,size)
+
+// console.log(whoWins(amount,inventory,price,size))
+
+
+// function totalCups(a){
+
+//     if(a>=6){
+//        let t= (a/6+a);
+//        return(Math.floor(t));
+//     }
+// }
+
+// let a=Number(prompt("enter amount ?"));
+// totalCups(a);
+// console.log(totalCups(a))
+
+
+// function limiIts(a ,b,c) {
+    
+//     for(i=b;i<=c;i++){
+//         if (a>b && a<c){
+//             return(a)
+//         }else if(a<b){
+//             return(b)
+//         }
+//     } return(c)
+// }
+
+// let a=Number(prompt("enter number ?"));
+// let b=1;
+// let c=10;
+// limiIts(a ,b,c)
+
+// console.log(limiIts(a ,b,c))
+
+
+
+// function POssibelpath(a) {
+//     let total= 1
+//     for(i=1;i<=a;i++){
+        
+//         total=total*i
+//     }
+//     return(total);
+    
+// }
+    
+// let a=Number(prompt("enter number ?"));
+// POssibelpath(a) 
+// console.log(POssibelpath(a) )
+
+
+
+// function MeanOfdigits(num) {
+//     let total = 0;
+//     while (num>0){
+//         let rem = num%10;
+//         total = total + rem; 
+//         num = Math.floor(num/10)
+//     }
+// }
+
+
+// function Count(n) {
+
+//     let total=1;
+//     while(n/10>=1){
+//         n/10;
+//         total=total+1;
+//         return(total)
+//     }
+    
+    
+// }
+
+// let n=153;
+// let k=Count(n)
+// console.log(k)
+
+
+// function digits_count(n) {
+//     let count = 0;
+//     if (n >= 1) 
+//     count=count+1
+  
+//     while (n/10>= 0) {
+//       n=n/10;
+//       count=count+1;
+//     }
+  
+//     return count;
+//   }
+
+//   let n=12345
+  
+//   console.log(digits_count(n));
